046eb5ec871040d8f0713376b264ef4841914c21
duongdaoq <duongdaoq@gmail.com>
2023-12-26 23:05:24 +0700

    all java exercise

--- src/Buoi4/B3/SinhVienNganhCo.java
+++
@@ -1,9 +0,0 @@
-package Buoi4.B3;
-
-import java.util.Date;
-
-public class SinhVienNganhCo extends SinhVien{
-    public SinhVienNganhCo(String name, String birthday, String birhtplace) {
-        super(name, birthday, birhtplace);
-    }
-}
--- src/Buoi4/B3/SinhVienNganhDien.java
+++
@@ -1,9 +0,0 @@
-package Buoi4.B3;
-
-import java.util.Date;
-
-public class SinhVienNganhDien extends SinhVien {
-    public SinhVienNganhDien(String name, String birthday, String birthplace){
-        super(name, birthday, birthplace);
-    }
-}
